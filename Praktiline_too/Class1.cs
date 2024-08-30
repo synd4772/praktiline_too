8da6841322d3baea79944e03d4e0ec822bfe3424
Copper <82279357+synd4772@users.noreply.github.com>
2024-08-30 10:48:23 +0300

    Add files via upload

---
+++ Praktiline_too/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PraktilineToo
+{
+    public class Korrutustabel 
+    {
+        public Korrutustabel()
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                for (int j = 1; j < 11; j++)
+                {
+                    Console.Write(i * j + " ");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
